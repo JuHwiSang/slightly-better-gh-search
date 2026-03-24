f326e8aa6be87989ee5d6b10dce1db0b19a4c4a5
JuHwiSang <juhs2005@gmail.com>
2026-03-24 23:18:58 +0900

    fix: remove deprecated options. use external for support node:crypto

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,10 +1,6 @@
 import type { LayoutServerLoad } from "./$types";
 import type { Config } from "@sveltejs/adapter-vercel";
 
-export const config: Config = {
-    runtime: "edge",
-};
-
 export const load: LayoutServerLoad = async ({ locals }) => {
     const { session } = await locals.safeGetSession();
     return { session };
