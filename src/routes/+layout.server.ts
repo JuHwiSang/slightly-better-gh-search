b7be25144b9bcc6f7279a95657694889c8896c14
JuHwiSang <juhs2005@gmail.com>
2026-03-24 23:07:41 +0900

    ci(DEV-71): use edge runtime for sveltekit

--- src/routes/+layout.server.ts
+++ src/routes/+layout.server.ts
@@ -1,4 +1,9 @@
 import type { LayoutServerLoad } from "./$types";
+import type { Config } from "@sveltejs/adapter-vercel";
+
+export const config: Config = {
+    runtime: "edge",
+};
 
 export const load: LayoutServerLoad = async ({ locals }) => {
     const { session } = await locals.safeGetSession();
